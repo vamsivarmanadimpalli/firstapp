9c8cb1835c2f25e9d343413de9968e2bcd01ed83
vamsivarmanadimpalli <vamsinadimpalli129@gmail.com>
2022-05-14 20:16:17 +0530

    create new functions

--- src/home/HomePage.js
+++ src/home/HomePage.js
@@ -1,40 +1,105 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import PublishIcon from '@material-ui/icons/Publish';
+import AddIcon from '@material-ui/icons/Add';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import DeleteOutlineSharpIcon from "@mui/icons-material/DeleteOutlineSharp";
+import EditIcon from '@mui/icons-material/Edit';
+import { GetApp, Home } from '@material-ui/icons';
+import './HomePage.css';
+import FormDialog from '../Components/FormDialog';
+import { Box, Modal } from '@material-ui/core';
 
-const HomePage = () => {
-  const [data, setData] = useState([]);
-  const getData = async () => {
-    let response = await
-      axios.get("https://jsonplaceholder.typicode.com/users");
-    setData(response.data);
-  };
-  useEffect(() => {
-    console.log("data",data);
-    getData();
-  }, [data]);
+const HomePage = (props) => {
+  const [open, setOpen] = React.useState(false);
 
+  const columnDefs = [
+    { headerName: "ID", field: "id" },
+    { headerName: "Country", field: "country", width: 10, cellRenderer: "iconComponent"},
+    { headerName: "Name", field: "name" },
+    // { headerName: "User Name", field: "username" },
+    { headerName: "Email", field: "email" },
+    { headerName: "Body", field: "body",edittable: true },
+   // { headerName: "Actions", edittable: true },
+    // { headerName: "Phone", field: "phone" },
+    // { headerName: "Company", field: "company.name" },
+    // { headerName: "Street", field: "address.street" },
+    // { headerName: "Suite", field: "address.suite" },
+    // { headerName: "City", field: "address.city" },
+    // { headerName: "Zip-Code", field: "address.zipcode" }
+  ]
+  const defaultColDef = {
+    sortable: true,
+    flex: 1, filter: true,
+    floatingFilter: true
+  }
+  let obj={};
+  const [data,setData]=useState(obj);
+ 
+
+  const refreshpage=()=>{
+window.location.reload();
+  }
+
+function deleteRow(force = false) {
+  let data = props.data;
+  let confirm = true;
+  if (!force) {
+    confirm = window.confirm(`are you sure you want to delete this row: ${JSON.stringify (data)}`)
+  }
+  if (confirm) {
+    props.api.updateRowData({remove:[data]});
+    props.api.refreshCells({force:true});
+  }
+};
+
+
+
+    const onGridReady = (params)=>{
+console.log("grid id ready")
+fetch("https://jsonplaceholder.typicode.com/comments").then(resp=>resp.json())
+.then(resp=>{console.log(resp)
+  params.api.applyTransaction({add:resp})})
+  }
+  function IconComponent(props) {
+    return (<><EditIcon />  <DeleteOutlineSharpIcon width={13} onClick={() => deleteRow()} /></>);
+  }
+
+  const [isFormDialogModalOpen, setFormDialogModalState] = useState(false);
+  const handleFormDialogModal = (value) => setFormDialogModalState(value);
+
+// const formDialogCallback = () => {
+//   handleFormDialogModal(false);
+// }
   return (
-    <>
-      {data && data.length > 0 && data.map((item) => {
-        return (<div>
-          {item.id}
-          {item.name}
-         {item.username}
-          {item.email}
-          {/* {item.address} */}
-         {item.street}
-          {item.suite}
-          {item.city}
-          {item.zipcode}
-          {item.geo}
-          {item.lat}
-          {item.lng}
-          {item.phone}
-          {item.website}
-        </div>)
-      }
-      )}
-    </>
+    <div className="App">
+      <h1 >React-App</h1>
+      <div style={{ textAlign: 'right' }} >
+        <button className='product_btns'><GetApp /></button>
+        <button className='product_btns'  onClick={() => setFormDialogModalState(true)}><AddIcon /></button>
+        <button className='product_btns' onClick={refreshpage}><RefreshIcon /></button>
+        <button className='product_btns' ><PublishIcon /></button>
+      </div>
+      <div className="ag-theme-alpine" style={{ height: '400px' }}>
+      <AgGridReact 
+      frameworkComponents={{
+        iconComponent: IconComponent
+      }}
+      columnDefs={columnDefs}
+      defaultColDef={defaultColDef}
+      onGridReady={onGridReady}>
+      </AgGridReact>
+      </div>
+      <Modal
+      open={isFormDialogModalOpen}
+      onClose={() => handleFormDialogModal(false)}
+      >
+      {/* <FormDialog onClick={formDialogCallback} /> */}
+      <FormDialog />
+      </Modal>
+    </div >
   );
 }
 export default HomePage;
